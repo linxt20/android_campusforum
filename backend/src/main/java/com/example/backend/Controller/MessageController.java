eadea8c19f7ff7b2f5897613ba4b6160e750ed64
shuibuxingha <15510682898@163.com>
2023-05-17 00:36:16 +0800

    前端给后端传文件 前端代码在TestImageActivity里，但因为没写跳转进不了这个页面

--- backend/src/main/java/com/example/backend/Controller/MessageController.java
+++ backend/src/main/java/com/example/backend/Controller/MessageController.java
@@ -1,17 +1,30 @@
 package com.example.backend.Controller;
 
 import com.example.backend.Base.Message;
+
+import jdk.internal.loader.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+
 
 @RestController
 public class MessageController {
@@ -32,19 +45,47 @@ public class MessageController {
         }
     }
 
-    @PostMapping("/upload-image")
+    @PostMapping("/test_image")
     public ResponseEntity<String> uploadImage(@RequestParam("image") MultipartFile file) {
+        //++++端给后端传文件
+        if (file.isEmpty()) {
+            return ResponseEntity.badRequest().body("No image file provided");
+        }
         try {
-            // 保存文件到本地或数据库
+            System.out.println("Start to upload");
+            // 指定存储文件的目录
+            String uploadDirectory = "D:\\astudy\\2023spring\\Android\\bighw\\android_campusforum\\backend\\image";
+            // 获取文件名
+            String fileName = file.getOriginalFilename();
+            // 构建存储文件的路径
+            String filePath = uploadDirectory + File.separator + fileName;
+            // 将文件保存到本地
+            file.transferTo(new File(filePath));
             // 返回成功状态码和文件保存的URL
-            return ResponseEntity.ok("http://your-server.com/image/" + file.getOriginalFilename());
-        } catch (Exception e) {
+            return ResponseEntity.ok("Image uploaded successfully. URL: http://127.0.0.1/images/" + fileName);
+        } catch (IOException e) {
             e.printStackTrace();
             // 返回错误状态码和错误信息
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body("Failed to upload image: " + e.getMessage());
         }
     }
+
+
+
+//    @PostMapping("/upload-image")
+//    public ResponseEntity<String> uploadImage(@RequestParam("image") MultipartFile file) {
+//        try {
+//            // 保存文件到本地或数据库
+//            // 返回成功状态码和文件保存的URL
+//            return ResponseEntity.ok("http://your-server.com/image/" + file.getOriginalFilename());
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            // 返回错误状态码和错误信息
+//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+//                    .body("Failed to upload image: " + e.getMessage());
+//        }
+//    }
 
 
     @PostMapping("/test_data")
